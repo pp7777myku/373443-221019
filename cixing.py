233483c8dd462d4b932ca1cd93349262b7f9933b
pp7777myku <62941440+pp7777myku@users.noreply.github.com>
2022-11-20 18:25:11 +0300

    Update cixing.py

--- cixing.py
+++ cixing.py
@@ -1,7 +1,7 @@
 import requests
 import nltk
 import numpy as np
-response = requests.get("https://gist.githubusercontent.com/nzhukov/b66c831ea88b4e5c4a044c952fb3e1ae/raw/7935e52297e2e85933e41d1fd16ed529f1e689f5/A%2520Brief%2520History%2520of%2520the%2520Web.txt")
+response = requests.get("https://gist.githubusercontent.com/pp7777myku/d2597707854dd8d96db736bdedcce00b/raw/b29a9d860604e2fe8231ab38ad91538cc9e19a38/gistfile1.txt")
 text = response.text
 text_tokenized = nltk.tokenize.word_tokenize(text)
 tagged_words = nltk.pos_tag(text_tokenized)
